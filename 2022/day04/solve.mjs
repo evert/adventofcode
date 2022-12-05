908641caab516070bfb4e9ed74b638682e41dab2
Evert Pot <me@evertpot.com>
2022-12-04 22:53:23 -0500

    day 4 ez

---
+++ 2022/day04/solve.mjs
@@ -0,0 +1,22 @@
+import { readInput, commonChars } from '../../helpers.js';
+
+const input = readInput();
+
+const pairs = input.split('\n').map(
+  item => 
+    item.match(/^([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)$/)
+      .slice(1)
+      .map( num => +num )
+);
+
+const part1 = pairs.filter(
+  ([a1, a2, b1, b2]) => (a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2)
+).length;
+
+console.log('Part 1: %i', part1);
+
+const part2 = pairs.filter(
+  ([a1, a2, b1, b2]) => (a1 <= b2 && a2 >= b1) || (b1 <= a2 && b2 >= a1) 
+).length;
+
+console.log('Part 2: %i', part2);
